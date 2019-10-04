358bc18172a4f1bc1e95ad9383fd5b734302a9bd
Vladislav Krivokon <vladeslimonade@gmail.com>
2019-10-05 00:08:58 +0300

    Updates

---
+++ precache-manifest.79171e6d8778cfe0abb0e4e631b9777f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c482223765dcc7de4c81618b2c813a31",
+    "url": "/shop-spa/index.html"
+  },
+  {
+    "revision": "d8463b6793c0a8582da2",
+    "url": "/shop-spa/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2b0aae39c815e0672d93",
+    "url": "/shop-spa/static/js/2.a313ed5f.chunk.js"
+  },
+  {
+    "revision": "d8463b6793c0a8582da2",
+    "url": "/shop-spa/static/js/main.2032cf0f.chunk.js"
+  },
+  {
+    "revision": "b1ce8c794ca3347ce835",
+    "url": "/shop-spa/static/js/runtime-main.d57470f1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/shop-spa/static/media/logo.25bf045c.svg"
+  }
+]);
